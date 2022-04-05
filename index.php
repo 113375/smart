4a149e7f7aad180af1fe03264b038b012aaea5e5
113375 <knyazevdima05@gmail.com>
2022-04-05 21:09:14 +0300

    changed header

--- index.php
+++ index.php
@@ -11,23 +11,26 @@
 </head>
 
 <body>
+
   <?php include "parts/header.php" ?>
+  <div class="container">
 
-  <div class="tabs__content">
-    <div class="tab is-active" block-content-name="profile_page">
+    <div class="tabs__content">
+      <div class="tab is-active" block-content-name="profile_page">
 
-      Профили
-      <!-- TODO доделать раздел  с профилями, добавить создание новых профилей -->
+        Профили
+        <!-- TODO доделать раздел  с профилями, добавить создание новых профилей -->
 
-    </div>
-    <div class="tab" block-content-name="lamp-page">
-      Лампочки
-      <!-- TODO доделать раздел  с лампочкой -->
-    </div>
+      </div>
+      <div class="tab" block-content-name="lamp-page">
+        Лампочки
+        <!-- TODO доделать раздел  с лампочкой -->
+      </div>
 
-    <div class="tab" block-content-name="history_page">
-      История проходов
-      <!-- TODO Выводить историю проходов по замку -->
+      <div class="tab" block-content-name="history_page">
+        История проходов
+        <!-- TODO Выводить историю проходов по замку -->
+      </div>
     </div>
   </div>
 
